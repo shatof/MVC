e8978ad950d08ca79ccb14c7ad6041ff2ad811af
Shaïn Atof <shain.atof@etu.univ-nantes.fr>
2024-11-13 23:46:35 +0100

    changes

--- js/controler.js
+++ js/controler.js
@@ -1,53 +1,17 @@
-class UpdateNbInputs extends Observer {
-  constructor(view) {
-    super();
-    this.view = view;
-  }
-
-  update(observable) {
-    // Clear existing inputs
-    observable.inputs.forEach(input => this.view.div.removeChild(input));
-    observable.inputs = [];
-
-    for (let i = 0; i < observable.nbInputs; i++) {
-      let input = document.createElement('input');
-      input.type = 'text';
-      this.view.div.appendChild(input);
-      observable.inputs.push(input);
-
-      input.addEventListener('input', (event) => {
-        observable.setTexte(input.value);
-      });
-    }
-  }
-}
-
 class UpdateRemplissage extends Observer {
   constructor(view) {
     super();
     this.view = view;
-    this.villes = [
-      "Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Strasbourg", "Montpellier", "Bordeaux", "Lille",
-      "Rennes", "Reims", "Le Havre", "Saint-Étienne", "Toulon", "Angers", "Grenoble", "Dijon", "Nîmes", "Aix-en-Provence",
-      "Saint-Quentin", "Brest", "Limoges", "Tours", "Amiens", "Metz", "Perpignan", "Besançon", "Orléans", "Rouen",
-      "Mulhouse", "Caen", "Nancy", "Argenteuil", "Saint-Denis", "Montreuil", "Roubaix", "Dunkerque", "Tourcoing",
-      "Nanterre", "Avignon", "Créteil", "Poitiers", "Versailles", "Pau", "Courbevoie", "Vitry-sur-Seine", "Colombes", "Aulnay-sous-Bois"
-    ];
   }
 
   update(observable) {
-    this.view.champRecherche.addEventListener('input', (event) => {
-      let input = event.target.value.toLowerCase();
-      let suggestions = this.villes.filter(ville => ville.toLowerCase().startsWith(input));
-      this.displaySuggestions(suggestions);
-    });
-  }
-
-  displaySuggestions(suggestions) {
     this.view.suggestionsList.innerHTML = ''; // Clear previous suggestions
-    suggestions.forEach(suggestion => {
+    observable.listeVilles.forEach(ville => {
       let li = document.createElement('li');
-      li.textContent = suggestion;
+      li.textContent = ville;
+      li.addEventListener('click', () => {
+        observable.setVille(ville);
+      });
       this.view.suggestionsList.appendChild(li);
     });
   }
@@ -60,9 +24,7 @@ class UpdateVille extends Observer {
   }
 
   update(observable) {
-    if (observable.ville) {
       this.view.resVille.textContent = `Vous avez sélectionné la ville : ${observable.ville}`;
-    }
   }
 }
 
@@ -73,9 +35,6 @@ class Controler {
     this.view = new View();
     this.model = model;
 
-    // update
-    let updateNbInputs = new UpdateNbInputs(this.view);
-    this.model.addObservers(updateNbInputs);
 
     let updateRemplissage = new UpdateRemplissage(this.view);
     this.model.addObservers(updateRemplissage);
@@ -85,82 +44,41 @@ class Controler {
 
     //  action
 
-
-    let actionCliqueMDR = (event) => {
-      //changeTexte();
-      actionChangeNbInputs();
+    
+    let actionChangeSuggestions = (event) => {
+      this.model.updateSuggestions(event.target.value);
     }
 
-    // let changeTexte = (event) => {
-    //   this.model.setTexte(this.view.champRNG.value);
-    //  }
-
-    let actionChangeNbInputs = (event) => {
-      this.model.setNbInputs(this.view.nombreInputs.value);
-    }
-
-    this.view.boutonMDR.addEventListener('click', actionCliqueMDR);
-    // this.view.boutonPlus.addEventListener('click', actionPlus);
-    // this.view.boutonMoins.addEventListener('click', actionMoins);
+    
+    this.view.champRecherche.addEventListener('input', actionChangeSuggestions);
 
   }
 }
 
-// class UpdateTxt extends Observer {
+// class UpdateNbInputs extends Observer {
 //   constructor(view) {
 //     super();
 //     this.view = view;
 //   }
 
 //   update(observable) {
-//     this.view.champTexte.value = observable.valeurDeBase;
-//   }
-// }
+//     // Clear existing inputs
+//     observable.inputs.forEach(input => this.view.div.removeChild(input));
+//     observable.inputs = [];
 
-// class UpdateDisable extends Observer {
-//   constructor(view) {
-//     super()
-//     this.view = view;
-//   }
+//     for (let i = 0; i < observable.nbInputs; i++) {
+//       let input = document.createElement('input');
+//       input.type = 'text';
+//       this.view.div.appendChild(input);
+//       observable.inputs.push(input);
 
-//   update(observable) {
-//     if (observable.valeurDeBase == Model.MAX) {
-//       this.view.boutonPlus.disabled = true;
-//     } else {
-//       this.view.boutonPlus.disabled = false;
-//     }
-//     if (observable.valeurDeBase == Model.MIN) {
-//       this.view.boutonMoins.disabled = true;
-//     } else {
-//       this.view.boutonMoins.disabled = false;
+//       input.addEventListener('input', (event) => {
+//         observable.setTexte(input.value);
+//       });
 //     }
 //   }
 // }
 
-// class UpdateHey extends Observer {
-//   constructor(view) {
-//     super();
-//     this.view = view;
-//   }
-
-//   update(observable) {
-//     this.view.res.textContent = observable.textent;
-//   }
-// }
-
-    // let updateTxt = new UpdateTxt(this.view);
-    // this.model.addObservers(updateTxt);
-
-    // let updateDisable = new UpdateDisable(this.view);
-    // this.model.addObservers(updateDisable);
-
-    // let updateHey = new UpdateHey(this.view);
-    // this.model.addObservers(updateHey);
-
-        // let actionPlus = (event) => {
-    //   this.model.plus();
-    // }
-
-    // let actionMoins = (event) => {
-    //   this.model.moins();
+  // let actionChangeNbInputs = (event) => {
+    //   this.model.setNbInputs(this.view.nombreInputs.value);
     // }
--- js/model.js
+++ js/model.js
@@ -2,59 +2,48 @@ class Model extends Observable {
     static MAX = 10;
     static MIN = 0;
 
-    constructor(){
+    constructor() {
         super();
-        // this.valeurDeBase = 0;
-        // this.textent = '';
-        this.nbInputs = 0;
+        //this.nbInputs = 0;       
+        //this.inputs = [];
         this.ville = '';
-        this.inputs = [];
+        this.listeVilles = [
+            "Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Strasbourg", "Montpellier", "Bordeaux", "Lille",
+            "Rennes", "Reims", "Le Havre", "Saint-Étienne", "Toulon", "Angers", "Grenoble", "Dijon", "Nîmes", "Aix-en-Provence",
+            "Saint-Quentin", "Brest", "Limoges", "Tours", "Amiens", "Metz", "Perpignan", "Besançon", "Orléans", "Rouen",
+            "Mulhouse", "Caen", "Nancy", "Argenteuil", "Saint-Denis", "Montreuil", "Roubaix", "Dunkerque", "Tourcoing",
+            "Nanterre", "Avignon", "Créteil", "Poitiers", "Versailles", "Pau", "Courbevoie", "Vitry-sur-Seine", "Colombes", "Aulnay-sous-Bois"
+          ];
     }
 
-    setNbInputs(nb) {
-        this.nbInputs = nb;
+    setListeVilles(listeVilles) {
+        this.listeVilles = listeVilles;
         this.setChanged();
         this.notifyObservers();
     }
 
-    setInputs(inputs) {
-        this.inputs = inputs;
-        this.setChanged();
-        this.notifyObservers();
-    }
+
+    updateSuggestions(input) {
+        let suggestions = this.listeVilles.filter(ville => ville.toLowerCase().startsWith(input.toLowerCase()));
+        this.setListeVilles(suggestions);
+      }
 
     setVille(ville) {
         this.ville = ville;
         this.setChanged();
         this.notifyObservers();
     }
-
-
-
-
-    // setValeurDeBase(val){
-    //     if (val <= Model.MAX && val >= Model.MIN){
-    //     this.valeurDeBase = val;
+    
+    // setNbInputs(nb) {
+    //     this.nbInputs = nb;
     //     this.setChanged();
     //     this.notifyObservers();
-    //     }
     // }
 
-    // plus() {
-    //     this.setValeurDeBase(this.valeurDeBase + 1);
-    // }
-
-    // moins() {
-    //     this.setValeurDeBase(this.valeurDeBase - 1);
-    // }
-
-    // setTexte(texte) {
-    //     this.textent = texte.concat(', voici est le mot que tu as choisi.');
+    
+    // setInputs(inputs) {
+    //     this.inputs = inputs;
     //     this.setChanged();
     //     this.notifyObservers();
-    //   }
-
-
-
-
+    // }
 }
